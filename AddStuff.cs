3f62349a9a577f9cd774b60a759b8ffb3e4ae346
bigpalooka <procyk01@gmail.com>
2017-11-18 23:41:38 -0500

    Add project files.

---
+++ AddStuff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class AddStuff
+{
+	public AddStuff()
+	{
+        //Console.Write("This is the answer: " + i + j);
+	}
+
+    public void jpAdd(int i, int j)
+    {
+        int jp = i + j;
+        Console.WriteLine("This is the answer: " + jp);
+        
+    }
+}
---
+++ ConsoleApp11172017a/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// using System.IO;
+
+
+
+namespace ConsoleApp11172017a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j;
+            int k;
+
+            i = Convert.ToInt16(Console.ReadLine());
+            j = Convert.ToInt16(Console.ReadLine());
+            k = i + j;
+
+            Console.WriteLine(k);
+
+            AddStuff az = new AddStuff();
+            az.jpAdd(i, j);
+
+            
+            Console.ReadKey();
+
+        }
+    }
+}
