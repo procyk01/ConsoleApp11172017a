6b9233f07daaa5f8a71b5719f8f05d0d3be46fd7
bigpalooka <procyk01@gmail.com>
2019-04-05 23:55:35 -0400

    added constructor write()

--- AddStuff.cs
+++ AddStuff.cs
@@ -5,6 +5,7 @@ public class AddStuff
 	public AddStuff()
 	{
         //Console.Write("This is the answer: " + i + j);
+        Console.Write("In AddStuff()");
 	}
 
     public void jpAdd(int i, int j)
